fd66fa442d5e7781233b3444b99f1a01d2274020
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-06 18:56:57 +0200

    added javascript for a basic modal, opening and closing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-// write code here...
+// get the modal element
+let modal = document.querySelector("#simpleModal");
+// get open modal button
+let modalBtn = document.querySelector("#modalBtn");
+// get close button
+let closeBtn = document.querySelector(".closeBtn");
+
+// listen for click
+modalBtn.addEventListener('click', openModal);
+
+// function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+// listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+// listen for outside click
+window.addEventListener('click', clickOutside);
+
+// function to close modal if outside click
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
